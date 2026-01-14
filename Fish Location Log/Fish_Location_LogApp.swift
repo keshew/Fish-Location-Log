b67c62f8d929e20c5d717aed96b42bec2882b801
keshew <artyom.korotckov2017@yandex.ru>
2026-01-14 15:32:34 +0500

    Initial Commit

---
+++ Fish Location Log/Fish_Location_LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fish_Location_LogApp.swift
+//  Fish Location Log
+//
+//  Created by Артём Коротков on 14.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct Fish_Location_LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
